7ff581bef1fb8e12347082041d192933b88e32a7
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-15 18:05:04 +0300

    feat: retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,12 +25,24 @@ def call_history(method: Callable) -> Callable:
     """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        _input = str(args)
-        self._redis.rpush(method.__qualname__ + ':inputs', _input)
+        inputs = str(args)
+        self._redis.rpush(method.__qualname__ + ':inputs', inputs)
         output = str(method(self, *args, **kwargs))
         self._redis.rpush(method.__qualname__ + ':outputs', output)
         return output
     return wrapper
+
+
+def replay(method: Callable) -> None:
+    """Display the history of calls of a particular function."""
+    name = method.__qualname__
+    cache = redis.Redis()
+    calls = cache.get(name).decode('utf-8')
+    print(f"{name} was called {calls} times")
+    inputs = cache.lrange(name + ':inputs', 0, -1)
+    outputs = cache.lrange(name + ':outputs', 0, -1)
+    for i, o in zip(inputs, outputs):
+        print(f"{name}(*{i.decode('utf-8')}) -> {o.decode('utf-8')}")
 
 
 class Cache:
